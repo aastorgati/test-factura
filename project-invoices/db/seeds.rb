34872658a656422322fad20064b8359172e79a5d
aastorgati <alexis.astorga.ti@gmail.com>
2021-04-27 21:21:43 -0400

    Add api

--- project-invoices/db/seeds.rb
+++
@@ -1,3 +0,0 @@
-User.create!(email: 'test@ksec.cl', password: 'admin123')
-category = Category.create!(code: 'c1', name: 'lacteos')
-Product.create!(code: 'p1', name: 'leche', brand: 'colun', count: 1, price: 900, category_id: category.id)
